846d8fce61ea120b4d444d1c305c305b194fa1ae
UltraViolet33 <93035200+UltraViolet33@users.noreply.github.com>
2023-10-10 11:49:08 +0200

    add title-center class to h2 link title

--- app/inc/link.php
+++ app/inc/link.php
@@ -1,8 +1,9 @@
 <div class="link">
-  <h2 class="center"><a href="<?= $link->link ?>"><?= $link->title ?></a></h2>
+  <h2 class="center link-title"><a href="<?= $link->link ?>"><?= $link->title ?></a></h2>
   <?php foreach ($link->item as $entry) : ?>
     <div class="items">
       <h3> <a href="<?= $entry->link ?>"> <?= $entry->title; ?></a></h3>
     </div>
+    <hr>
   <?php endforeach; ?>
 </div>
